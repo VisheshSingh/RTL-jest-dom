0336b30e27f775fa7f6ce3f11b63321a575035b2
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-25 15:13:00 -0500

    cleanup 🧹🧹

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
+
+afterEach(cleanup);
 
 describe('Testing App Component', () => {
   test('Should render App component with any errors', () => {
