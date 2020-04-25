7ebf91a9d954bc1899b721582106651d2004d807
Vishesh Singh <VisheshSingh16@gmail.com>
2020-04-25 16:04:23 -0500

    testing events 👇👇

--- src/components/TestElements.test.js
+++ src/components/TestElements.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import TestElements from './TestElements';
 
 // afterEach(cleanup);
@@ -7,6 +7,22 @@ import TestElements from './TestElements';
 describe('Test Counter functionality', () => {
   it('should equal to 0', () => {
     const { getByTestId } = render(<TestElements />);
+    const counter = getByTestId('counter');
+    expect(counter).toHaveTextContent(0);
+  });
+
+  it('should increment by 1', () => {
+    const { getByTestId } = render(<TestElements />);
+    const buttonUp = getByTestId('button-up');
+    fireEvent.click(buttonUp);
+    const counter = getByTestId('counter');
+    expect(counter).toHaveTextContent(1);
+  });
+
+  it('should decrement by 1', () => {
+    const { getByTestId } = render(<TestElements />);
+    const buttonDown = getByTestId('button-down');
+    fireEvent.click(buttonDown);
     const counter = getByTestId('counter');
     expect(counter).toHaveTextContent(0);
   });
